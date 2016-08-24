fa6948c1a13ae45c7586c28ba999939d8062fab7
root <root@2f23f4b2e87a>
2016-08-24 09:36:32 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "ProfileSettings",
+  "homePage" : "Main",
+  "name" : "ProfileSettings",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
