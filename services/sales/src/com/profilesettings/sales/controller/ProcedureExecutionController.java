fa8951bb57986a183f6a8f46958d1fe7a676ece8
Tejaswi maryala <tejaswi.maryala+1@wavemaker.com>
2016-08-25 09:39:06 +0000

    New files added - [ProductcatalogServiceImpl.java,AccountManagerController.java,ClientService.java,VenueDetail.java,VenueDetailServiceImpl.java,AccountManager.java,sales_published_dataModel.json,SalesProcedureExecutorServiceImpl.java,SaleService.java,sales.spring.xml,YearServiceImpl.java,YearDao.java,Client.java,AccountManagerService.java,ProductcatalogController.java,ClientDao.java,SaleController.java,dbprofile.properties,ActionItemServiceImpl.java,ActionItemController.java,ActionItemDao.java,YearController.java,servicedef.xml,VenueDetailDao.java,ActionItemService.java,sales_query.json,SalesQueryExecutorServiceImpl.java,Year.java,YearService.java,ActionItem.java,SaleServiceImpl.java,AccountManagerServiceImpl.java,package-info.java,hrdb.properties,sales-queries.hbm.xml,SalesProcedureExecutorService.java,ProcedureExecutionController.java,VenueDetailController.java,QueryExecutionController.java,sales.properties,AccountManagerDao.java,ProductcatalogService.java,SalesQueryExecutorService.java,db-connection-settings.json,ClientServiceImpl.java,SaleDao.java,hrdb.script,sales-procedures.mappings.json,Sale.java,ProductcatalogDao.java,sales_procedure.json,VenueDetailService.java,ClientController.java,Productcatalog.java] Files modified - [footer.variables.json,rightnav.variables.json,project-services.xml,Common.variables.json,.wmproject.properties,Login.variables.json,app.variables.json,topnav.variables.json,Main.variables.json,leftnav.variables.json,production.properties,header.variables.json,Login.html,topnav.html,pom.xml,index.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/sales/src/com/profilesettings/sales/controller/ProcedureExecutionController.java
@@ -0,0 +1,45 @@
+/*Copyright (c) 2016-2017 wavemaker.com All Rights Reserved.
+ This software is the confidential and proprietary information of wavemaker.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with wavemaker.com*/
+package com.profilesettings.sales.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Qualifier;
+import com.profilesettings.sales.service.SalesProcedureExecutorService;
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+import com.wordnik.swagger.annotations.*;
+import com.wavemaker.tools.api.core.annotations.WMAccessVisibility;
+import com.wavemaker.tools.api.core.models.AccessSpecifier;
+
+@RestController(value = "Sales.ProcedureExecutionController")
+@RequestMapping("/sales/procedureExecutor")
+@Api(description = "Controller class for procedure execution", value = "ProcedureExecutionController")
+public class ProcedureExecutionController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcedureExecutionController.class);
+
+    @Autowired
+    private SalesProcedureExecutorService procedureService;
+
+    @RequestMapping(value = "/procedure/execute/wm_custom", method = RequestMethod.POST)
+    @WMAccessVisibility(value = AccessSpecifier.APP_ONLY)
+    @ApiOperation(value = "Process request to execute custom Procedure")
+    public List<Object> executeWMCustomProcedure(@RequestBody CustomProcedure procedure) {
+        List<Object> result = procedureService.executeWMCustomProcedure(procedure);
+        LOGGER.debug("got the result {}" + result);
+        return result;
+    }
+}
---
+++ services/sales/src/com/profilesettings/sales/service/SalesProcedureExecutorService.java
@@ -0,0 +1,22 @@
+/*Copyright (c) 2016-2017 wavemaker.com All Rights Reserved.
+ This software is the confidential and proprietary information of wavemaker.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with wavemaker.com*/
+
+
+package com.profilesettings.sales.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface SalesProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
